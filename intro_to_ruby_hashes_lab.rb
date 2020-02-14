aeedbc5daf2d22f609a8d2eea410f40221f47dcc
Matt Kuras <mattkuras@gmail.com>
2020-02-14 20:01:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,9 +23,7 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  if hash[key]
-    hash[key] +=1 
-  else 
+  if hash[key] = false 
     hash[key] = 1
 end
   # given a hash an a key as parameters, return an updated hash
